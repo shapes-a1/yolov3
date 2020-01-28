fbed4c36663c36fb16d0b90c452a1afc8931f21e
Raj <rajshah@outlook.ie>
2020-01-28 15:30:55 +0000

    fix script name

--- fasterrcnn.sh
+++ fasterrcnn.sh
@@ -2,6 +2,6 @@ for i in 0.6 0.7 0.8
 do
   for j in 416 608
   do
-    python detect.py --conf-thres $i --img-size $j --output fasterrcnn_${j}_conf${i} --source ../input_videos/client_london_police/CCTV\ footage\ of\ banned\ driver\ driving\ dangerously\ through\ London.mp4
+    python detect_fasterrcnn.py --conf-thres $i --img-size $j --output fasterrcnn_${j}_conf${i} --source ../input_videos/client_london_police/CCTV\ footage\ of\ banned\ driver\ driving\ dangerously\ through\ London.mp4
   done
 done
