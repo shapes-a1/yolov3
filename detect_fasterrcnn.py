a946b27beeff0a44b82a41684b447e1414648e13
Raj <rajshah@outlook.ie>
2020-01-28 15:05:30 +0000

    new loc of names

--- detect_fasterrcnn.py
+++ detect_fasterrcnn.py
@@ -97,8 +97,8 @@ def detect(save_img=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--names', type=str, default='data/coco_pt.names', help='*.names path')
-    parser.add_argument('--source', type=str, default='data/samples', help='source')  # input file/folder, 0 for webcam
+    parser.add_argument('--names', type=str, default='./coco_pt.names', help='*.names path')
+    parser.add_argument('--source', type=str, help='source')  # input file/folder, 0 for webcam
     parser.add_argument('--output', type=str, default='output', help='output folder')  # output folder
     parser.add_argument('--img-size', type=int, default=416, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.6, help='object confidence threshold')
