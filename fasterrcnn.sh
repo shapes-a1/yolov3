486512095b2657dd50910625f4218d0ba954861f
Raj <rajshah@outlook.ie>
2020-01-28 15:30:14 +0000

    automate res

---
+++ fasterrcnn.sh
@@ -0,0 +1,7 @@
+for i in 0.6 0.7 0.8
+do
+  for j in 416 608
+  do
+    python detect.py --conf-thres $i --img-size $j --output fasterrcnn_${j}_conf${i} --source ../input_videos/client_london_police/CCTV\ footage\ of\ banned\ driver\ driving\ dangerously\ through\ London.mp4
+  done
+done
